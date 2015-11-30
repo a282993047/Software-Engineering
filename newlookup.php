fada93e892906532d2f2184570310a4c020a422e
xueqifang <282993047@qq.com>
2015-11-30 22:07:35 +0800

    2015/11/30/22:07

--- newlookup.php
+++ newlookup.php
@@ -8,13 +8,46 @@
 </head>
 <body>
 <?php
+function makeSql($key1='',$key2='',$key3='',$key4=''){//多关键字查询
+    $sql    =    'where bookname ';
+    if ($key1!=''){
+        $title1    =    "like '%$key1%'";
+        $sql.=$title1;
+    }
+    if ($key2!=''){
+        $title2    =    "like '%$key2%'";
+        $sql.='or bookname '.$title2;
+    }
+    if ($key3!=''){
+        $title3    =    "like '%$key3%'";
+        $sql.='or bookname '.$title3;
+    }
+    if ($key4!=''){
+        $title4    =    "like '%$key4%'";
+        $sql.='or bookname '.$title4;
+    }
+    if ($sql=='where bookname '){
+        return '';
+    }
+    return $sql;
+}
 $con = mysql_connect("localhost","root","");
 mysql_query("set names utf8");
 mysql_select_db("test", $con);
 $name = $_POST['query'];
-$sql = "select * from BOOK1 where bookname LIKE '%{$name}%' ";
-$sql1= "select * from CLOTH1 where clothname LIKE '%{$name}%' ";
-$sql2= "select * from LIFETHING1 where lifethingname LIKE '%{$name}%' ";
+$name1=split(' ',$name);
+//if(!isset($name1[1])) $name1[1]=' ';
+//if(!isset($name1[2])) $name1[2]=' ';
+//if(!isset($name1[3])) $name1[3]=' ';
+//var_dump($name1);//输出$name1
+//$sql = "select * from BOOK1 where bookname LIKE '%{$name}%' ";
+//var_dump(makeSql($name1));
+//echo makeSql($name1[0],$name1[1],$name1[2],$name1[3]);
+$name2=makeSql($name1[0],$name1[1],$name1[2],$name1[3]);
+//var_dump(makeSql($name2));
+$sql = "select * from BOOK1 $name2 ";
+$sql1= "select * from CLOTH1 $name2 ";
+$sql2= "select * from LIFETHING1 $name2 ";
 $result = mysql_query($sql);
 $result1 = mysql_query($sql1);
 $result2 = mysql_query($sql2);
