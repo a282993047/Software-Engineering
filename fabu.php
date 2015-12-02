28805ca651bf11ab49d07497ffc67b578e27a247
xueqifang <282993047@qq.com>
2015-12-02 22:59:18 +0800

    Merge branch 'master' of github.com:a282993047/Software-Engineering
    # Conflicts:
    #	newlookup.php
    12/2/22:58

--- fabu.php
+++ fabu.php
@@ -35,7 +35,7 @@
 			<div class="location">
 				<span class="breadcrumb">
 					<a href="homepage.php">华师大二手购物网站</a>
-					<i class="icon-arrow-right"></i>
+					<i class="glyphicon glyphicon-arrow-right"></i>
 					<a href="fabu.php">发布商品</a>
 				</span>
 			</div>
@@ -45,15 +45,15 @@
 		<div class="slogan middle">简单，快速，搞得定！</div>
 		<ul class="flow clearfix sep-big">
 			<li class="flow-item active"><label class="i-badge">1</label>选择类目</li>
+			<span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
 			<li class="flow-item"><label class="i-badge">2</label>填写信息</li>
-			<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+			<span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
 			<li class="flow-item"><label class="i-badge">3</label>完成发布</li>
 		</ul>
 	</div>
 	<script src="http://libs.baidu.com/jquery/2.1.4/jquery.min.js"></script>
 	<script src="./js/jquery.qrcode.min.js"></script>
 	<script src="./js/bootstrap.js"></script>
-	<script src="./js/index.89e1ea6e.js"></script><!-- 你这引用的是啥文件？？？ !>
 
 </body>
 </html>
